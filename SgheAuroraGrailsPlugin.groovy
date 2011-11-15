42bae0c0d422fc03fdb7e1e0efe929fd24520ed7
Bob Rullo <robert.rullo@sungardhe.com>
2011-11-14 20:32:00 -0500

    Rev'd to 0.0.11

--- SgheAuroraGrailsPlugin.groovy
+++ SgheAuroraGrailsPlugin.groovy
@@ -1,8 +1,8 @@
 class SgheAuroraGrailsPlugin {
     // the plugin version
-    def version = "0.0.10"
+    def version = "0.0.11"
     // the version or versions of Grails the plugin is designed for
-    def grailsVersion = "1.3.5 > *"
+    def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
     def dependsOn = [:]
     // resources that are excluded from plugin packaging
