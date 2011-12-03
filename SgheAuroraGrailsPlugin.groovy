86785d89d0322b00b8cbeb4f96acdb2b2decb0d3
Jay Kadiam <jayachandra.kadiam@sungard.com>
2011-12-03 14:21:07 +0530

    Upgraded to version 0.0.18

--- SgheAuroraGrailsPlugin.groovy
+++ SgheAuroraGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SgheAuroraGrailsPlugin {
     // the plugin version
-    def version = "0.0.17"
+    def version = "0.0.18"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
