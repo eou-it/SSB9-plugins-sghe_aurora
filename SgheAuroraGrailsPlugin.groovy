dd87da2846521a4cbc99642b487adb5bf9eba96d
Navein.Fernandes <Navein.Fernandes@sungard.com>
2011-12-06 12:34:54 +0530

    Update version number of plugin to 0.0.19.

--- SgheAuroraGrailsPlugin.groovy
+++ SgheAuroraGrailsPlugin.groovy
@@ -1,6 +1,6 @@
 class SgheAuroraGrailsPlugin {
     // the plugin version
-    def version = "0.0.18"
+    def version = "0.0.19"
     // the version or versions of Grails the plugin is designed for
     def grailsVersion = "1.3.7 > *"
     // the other plugins this plugin depends on
