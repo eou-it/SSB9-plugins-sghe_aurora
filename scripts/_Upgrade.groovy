3fafbb0faf3ecccecb9a39dc53dfec24b9917273
Navein Austin Fernandes <Navein.Fernandes@sungard.com>
2011-12-18 16:53:18 +0530

    HWF-352: Copyright information added/modified.

--- scripts/_Upgrade.groovy
+++ scripts/_Upgrade.groovy
@@ -1,3 +1,14 @@
+/*********************************************************************************
+ Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
+ This copyrighted software contains confidential and proprietary information of 
+ SunGard Higher Education and its subsidiaries. Any use of this software is limited 
+ solely to SunGard Higher Education licensees, and is further subject to the terms 
+ and conditions of one or more written license agreements between SunGard Higher 
+ Education and the licensee in question. SunGard is either a registered trademark or
+ trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
+ Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
+ Education in the U.S.A. and/or other regions and/or countries.
+ **********************************************************************************/
 //
 // This script is executed by Grails during application upgrade ('grails upgrade'
 // command). This script is a Gant script so you can use all special variables
