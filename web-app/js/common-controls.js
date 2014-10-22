8afc9d94f035d6a0867b8b931719dfdf201eee41
shrridevis <shrridevi.sreedharan@ellucian.com>
2014-10-22 12:32:04 +0530

    Revert "fixed an rtl defect in skip link"
    This reverts commit 1137aa20e05981c627b07cacc87e5073433c30c9.

--- web-app/js/common-controls.js
+++ web-app/js/common-controls.js
@@ -15,9 +15,8 @@
  */
 
 function SkipLink() {
-    var hideSide = direction == 'rtl' ? 'right' : 'left'
     var skipText = ResourceManager.getString("skip_link_text");
-    return $("<a href='#maincontent' style='position:absolute; " + hideSide + ":-10000px; top:auto; width:1px; height:1px; overflow:hidden;'>" + skipText + "</a>");
+    return $("<a href='#maincontent' style='position:absolute; left:-10000px; top:auto; width:1px; height:1px; overflow:hidden;'>" + skipText + "</a>");
 }
 
 function Button(id, label, callback, type) {
