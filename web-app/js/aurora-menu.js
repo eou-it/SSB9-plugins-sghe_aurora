fd0c4fd2e93408b10fa71cc17e6d0b307467625b
karthick <karthic.kumar@ellucian.com>
2015-05-12 10:05:17 +0530

    Caret symbol with scroll fix

--- web-app/js/aurora-menu.js
+++ web-app/js/aurora-menu.js
@@ -261,7 +261,7 @@ function ScrollableMenuTable(root, menuList) {
         function addBackButton(){
             var subMenuName = _fnGetSelectedMenuName();
             var backButton = "<li><div class='menu-item'>"
-                +"<div class='menu-back-icon'></div><div class='menu-subheader-text'><a href='#' id='backButton'> "+subMenuName+" </a></div>"
+                +"<div class='menu-back-icon'></div><div class='menu-subheader-text'><span title="+subMenuName+"><a href='#' id='backButton'> "+subMenuName+" </a></span></div>"
                 +"</div>"
             return backButton;
         };
