a473aaaa3b2e9ed69b9e3273b565a8b905b123e1
Ron Boukens <Ron.Boukens@ellucian.com>
2015-05-20 14:08:43 +0200

    EXTZ-791: Remove dependency on Left-to-Right module for Right-to-Left module.

--- grails-app/conf/AuroraResources.groovy
+++ grails-app/conf/AuroraResources.groovy
@@ -1,22 +1,10 @@
-/*********************************************************************************
- Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of 
- SunGard Higher Education and its subsidiaries. Any use of this software is limited 
- solely to SunGard Higher Education licensees, and is further subject to the terms 
- and conditions of one or more written license agreements between SunGard Higher 
- Education and the licensee in question. SunGard is either a registered trademark or
- trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher 
- Education in the U.S.A. and/or other regions and/or countries.
- **********************************************************************************/
+/*******************************************************************************
+Copyright 2009-2015 Ellucian Company L.P. and its affiliates.
+*******************************************************************************/
 
 modules = {
-    'aurora' {
+    'auroraCommon' {
         dependsOn "jquery"
-        defaultBundle environment == "development" ? false : "aurora"
-
-        resource url:[plugin: 'sghe-aurora', file: 'css/common-controls.css'], attrs:[media:'screen, projection']
-        resource url:[plugin: 'sghe-aurora', file: 'css/common-platform.css'], attrs:[media:'screen, projection']
 
         resource url:[plugin: 'sghe-aurora', file: 'js/utils.js']
         resource url:[plugin: 'sghe-aurora', file: 'js/config.js']
@@ -26,7 +14,17 @@ modules = {
         resource url:[plugin: 'sghe-aurora', file: 'js/common-platform.js']
     }
 
+    'aurora' {
+        dependsOn "auroraCommon"
+        defaultBundle environment == "development" ? false : "aurora"
+
+        resource url:[plugin: 'sghe-aurora', file: 'css/common-controls.css'], attrs:[media:'screen, projection']
+        resource url:[plugin: 'sghe-aurora', file: 'css/common-platform.css'], attrs:[media:'screen, projection']
+    }
+
     'auroraRTL' {
+        dependsOn "auroraCommon"
+
         resource url:[plugin: 'sghe-aurora', file: 'css/common-controls-rtl.css'], attrs:[media:'screen, projection']
         resource url:[plugin: 'sghe-aurora', file: 'css/common-platform-rtl.css'], attrs:[media:'screen, projection']
         resource url:[plugin: 'sghe-aurora', file: 'css/rtl.css'],       attrs:[media:'screen, projection']
