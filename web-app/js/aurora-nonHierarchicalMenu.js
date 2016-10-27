909140d899f8f7ed5e9c9bfb0678ee140336058d
asism <Asis.Mohanty@ellucian.com>
2016-10-27 17:39:33 +0530

    commit after modifying the aurora header

--- web-app/js/aurora-nonHierarchicalMenu.js
+++ web-app/js/aurora-nonHierarchicalMenu.js
@@ -259,9 +259,36 @@ ToolsMenu.initialize = function() {
     this.callbackPostItemClick = toggleToolsMenu;
     ControlBar.node.find('#tools').bind("click", toggleToolsMenu);
 
-
+    try{
+        if(angular.module("aboutModal")){
+            var dialogDiv = document.getElementById('dialogAppDiv');
+            dialogDiv.setAttribute("ng-app","dialogApp");
+            dialogDiv.setAttribute("ng-controller","ModalCtrl");
+            dialogDiv.innerHTML = "<xe-about-modal show='modalShown' api='aboutApi'></xe-about-modal>";
+            ToolsMenu.addItem(
+                "about",
+                $.i18n.prop("aurora.areas_label_about_title"),
+                "",
+                aboutDialogPopUp
+            );
+        }
+    } catch(e){
+        console.log('Not adding About menu item because aboutModal Module is not found in resource.');
+    }
 };
 
+function aboutDialogPopUp () {
+    var scope = angular.element(document.getElementById('dialogAppDiv')).scope();
+    if(!scope){
+        angular.element(document.getElementById('dialogAppDiv')).ready(function() {
+            angular.bootstrap(document.getElementById('dialogAppDiv'), ['dialogApp']);
+        });
+        scope = angular.element(document.getElementById('dialogAppDiv')).scope();
+    }
+    scope.$apply(function(){
+        scope.toggleModal();
+    })
+}
 ToolsMenu.closeMenu = function() {
     if (!$('#toolsCanvas').is(':hidden')) {
         $('#toolsCanvas').removeClass('tools-active');
