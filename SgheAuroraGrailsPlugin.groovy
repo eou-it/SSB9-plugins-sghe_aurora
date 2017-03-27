f6f42a34575535a53be43ab1dd3954f5887384ec
Eshaan_kumar <eshaan.kumar@ellucian.com>
2017-03-27 17:35:03 +0530

    fix for HRU-7149 copyright updated

--- SgheAuroraGrailsPlugin.groovy
+++ SgheAuroraGrailsPlugin.groovy
@@ -1,14 +1,6 @@
-/*********************************************************************************
- Copyright 2009-2011 SunGard Higher Education. All Rights Reserved.
- This copyrighted software contains confidential and proprietary information of
- SunGard Higher Education and its subsidiaries. Any use of this software is limited
- solely to SunGard Higher Education licensees, and is further subject to the terms
- and conditions of one or more written license agreements between SunGard Higher
- Education and the licensee in question. SunGard is either a registered trademark or
- trademark of SunGard Data Systems in the U.S.A. and/or other regions and/or countries.
- Banner and Luminis are either registered trademarks or trademarks of SunGard Higher
- Education in the U.S.A. and/or other regions and/or countries.
- **********************************************************************************/
+/*******************************************************************************
+Copyright 2009 - 2017 Ellucian Company L.P. and its affiliates.
+*******************************************************************************/
 class SgheAuroraGrailsPlugin {
     // the plugin version
     def version = "9.21"
