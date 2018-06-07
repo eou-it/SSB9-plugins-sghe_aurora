02f755c34a7981cdcd61edf31d99a5bd16561f0e
Eshaan_kumar <eshaan.kumar@ellucian.com>
2018-06-07 12:17:50 +0530

    adding application groovy file to sghe_aurora plugin

---
+++ grails-app/conf/application.groovy
@@ -0,0 +1,32 @@
+/*******************************************************************************
+ Copyright 2009-2018 Ellucian Company L.P. and its affiliates.
+ *******************************************************************************/
+// configuration for plugin testing - will not be included in the plugin zip
+ 
+
+grails.views.default.codec="none" // none, html, base64
+grails.views.gsp.encoding="UTF-8"
+
+// Uncomment and edit the following lines to start using Grails encoding & escaping improvements
+
+/* remove this line 
+// GSP settings
+grails {
+    views {
+        gsp {
+            encoding = 'UTF-8'
+            htmlcodec = 'xml' // use xml escaping instead of HTML4 escaping
+            codecs {
+                expression = 'html' // escapes values inside null
+                scriptlet = 'none' // escapes output from scriptlets in GSPs
+                taglib = 'none' // escapes output from taglibs
+                staticparts = 'none' // escapes output from static template parts
+            }
+        }
+        // escapes all not-encoded output at final stage of outputting
+        filteringCodecForContentType {
+            //'text/html' = 'html'
+        }
+    }
+}
+remove this line */
