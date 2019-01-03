92fef3f9b72624ee974e91a84f0a106e7fd08e59
Ankur Jain <ankur.jain@ellucian.com>
2019-01-03 15:25:33 +0530

    Removing unnecessary default mapping - HRU-8280

--- grails-app/controllers/sghe/aurora/UrlMappings.groovy
+++ grails-app/controllers/sghe/aurora/UrlMappings.groovy
@@ -3,11 +3,6 @@ package sghe.aurora
 class UrlMappings {
 
     static mappings = {
-        "/ssb/$controller/$action?/$id?(.$format)?"{
-            constraints {
-                // apply constraints here
-            }
-        }
 
         "/"(view:"/index")
         "500"(view:'/error')
