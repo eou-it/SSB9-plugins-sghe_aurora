1cacd26328519eb013477c878092d225b215de29
Ankur Jain <ankur.jain@ellucian.com>
2019-01-03 16:19:13 +0530

    Reverting to last commit as this fix will be available in different branch - HRU-8280

--- grails-app/controllers/sghe/aurora/UrlMappings.groovy
+++ grails-app/controllers/sghe/aurora/UrlMappings.groovy
@@ -3,6 +3,11 @@ package sghe.aurora
 class UrlMappings {
 
     static mappings = {
+        "/ssb/$controller/$action?/$id?(.$format)?"{
+            constraints {
+                // apply constraints here
+            }
+        }
 
         "/"(view:"/index")
         "500"(view:'/error')
