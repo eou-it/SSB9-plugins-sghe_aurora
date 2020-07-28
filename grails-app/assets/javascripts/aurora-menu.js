980a4f11ec5af359f74a56249e1423b709b4752c
Arun Ramachandra Upadhya <Arun.Upadhya@ellucian.com>
2020-07-28 17:23:47 +0530

    HRU-9547 implemented code review comments

--- grails-app/assets/javascripts/aurora-menu.js
+++ grails-app/assets/javascripts/aurora-menu.js
@@ -1,5 +1,5 @@
 /*********************************************************************************
- Copyright 2016 Ellucian Company L.P. and its affiliates.
+ Copyright 2016-2020 Ellucian Company L.P. and its affiliates.
  **********************************************************************************/
 
 function ScrollableMenuTable(root, menuList) {
